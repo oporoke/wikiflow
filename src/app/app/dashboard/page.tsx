d193729407577c66b5d785e8915b93d14e982f96
Oporo <oporo.ke@gmail.com>
2026-01-15 14:08:28 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -57,7 +57,7 @@ export default function DashboardPage() {
               <Button size="sm">Invite team members</Button>
             </Link>
           </CardContent>
-        </card>
+        </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Recent Pages</CardTitle>
